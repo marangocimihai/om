100277ea933c5b4b144232df00e8e498bf22afdd
Mihai <marangoci.mihai93@gmail.com>
2019-12-17 20:15:43 +0200

    added more notes

--- src/main/java/spring/Application.java
+++ src/main/java/spring/Application.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "springboot/controller")
+@ComponentScan
 @EntityScan(basePackages = "springboot/model")
 public class Application {
     public static void main(String[] args) {
