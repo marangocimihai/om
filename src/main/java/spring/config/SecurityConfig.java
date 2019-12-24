f23eeb923dd9df3210badb6c5e768f3e7044140d
Mihai <marangoci.mihai93@gmail.com>
2019-12-24 23:32:01 +0200

    added some Spring security examples

--- src/main/java/spring/config/SecurityConfig.java
+++ src/main/java/spring/config/SecurityConfig.java
@@ -10,9 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest()
-                .permitAll()
-                .and().csrf().disable();
+        http.authorizeRequests().antMatchers("/employee/find/**").hasRole("ADMIN").and().httpBasic();
+        http.csrf().disable();
     }
 }
