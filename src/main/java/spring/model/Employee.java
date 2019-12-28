9ed166ad4e8cfb91c28c00b69a115e41a09cefa5
Mihai <marangoci.mihai93@gmail.com>
2019-12-28 23:09:59 +0200

    code refinement

--- src/main/java/spring/model/Employee.java
+++ src/main/java/spring/model/Employee.java
@@ -9,18 +9,14 @@ import org.hibernate.annotations.UpdateTimestamp;
 import javax.persistence.*;
 import java.sql.Timestamp;
 
-@Entity
-@Table(name = "employee")
-@NamedQueries(value = {
-        @NamedQuery(name = "Employee.findById", query = "Select e from Employee e where e.id = :id"),
-        @NamedQuery(name = "Employee.findByWageLessThan", query = "Select e from Employee e where e.wage < :wage"),
-})
-@NamedStoredProcedureQuery(name = "Employee.getEmployeeByName", procedureName = "getEmployeeByName", resultClasses = Employee.class, parameters = {
-        @StoredProcedureParameter(name = "name", type = String.class, mode = ParameterMode.IN)
-})
 @Getter
 @Setter
 @NoArgsConstructor
+@Entity
+@Table(name = "employee")
+@NamedStoredProcedureQuery(name = "Employee.getEmployeeByName", procedureName = "getEmployeeByName", resultClasses = Employee.class, parameters = {
+        @StoredProcedureParameter(name = "name", type = String.class, mode = ParameterMode.IN)
+})
 public class Employee extends Person {
     @Column(name = "wage")
     private Double wage;
@@ -37,21 +33,12 @@ public class Employee extends Person {
 //    @JoinColumn(name = "department_id", nullable = false)
 //    private Department department;
 
-    public Employee(Double wage) {
-        this.wage = wage;
-    }
+//    public Employee(Double wage) {
+//        this.wage = wage;
+//    }
 
     @PrePersist
     public void checkWage() {
         this.wage = this.wage < 25 ? 30.0 : this.wage;
-    }
-
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "wage=" + wage +
-                ", cDate=" + cDate +
-                ", uDate=" + uDate +
-                "} " + super.toString();
     }
 }
