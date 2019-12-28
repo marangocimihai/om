7f82814619fb8ad149f63c2b61f513e52ce1a734
Mihai <marangoci.mihai93@gmail.com>
2019-12-28 13:46:36 +0200

    added property expansion usage example, moved template file to be compliant with thymeleaf requirements

--- src/main/java/spring/controller/HomeController.java
+++ src/main/java/spring/controller/HomeController.java
@@ -4,15 +4,21 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
     @Value("${spring.application.name}")
     String appName;
 
-    @GetMapping("/")
-    public String homePage(Model model) {
+    @Value("${spring.application.version}")
+    String appVersion;
+
+    @GetMapping
+    public String home(Model model) {
         model.addAttribute("appName", appName);
+        model.addAttribute("appVersion", appVersion);
         return "home";
     }
 }
