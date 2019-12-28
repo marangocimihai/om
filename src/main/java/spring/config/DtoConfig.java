8a2a7ce82cd2d6b9bb3aded68020a8d72fecec29
Mihai <marangoci.mihai93@gmail.com>
2019-12-28 23:01:42 +0200

    disabled spring security

--- src/main/java/spring/config/DtoConfig.java
+++ src/main/java/spring/config/DtoConfig.java
@@ -1,13 +1,23 @@
-package spring.config;
+package spring;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
 
 @Configuration
 public class DtoConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean(name = "entityManagerFactory")
+    public LocalEntityManagerFactoryBean entityManagerFactoryBean() {
+        LocalEntityManagerFactoryBean factory = new LocalEntityManagerFactoryBean();
+        factory.setPersistenceUnitName("om");
+        return factory;
     }
 }
