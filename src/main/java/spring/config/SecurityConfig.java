4b7728ccb9d3b42031f05c4214d9cb1d4a094eec
Mihai <marangoci.mihai93@gmail.com>
2019-12-29 11:34:02 +0200

    disabled Spring security temporarily

--- src/main/java/spring/config/SecurityConfig.java
+++ src/main/java/spring/config/SecurityConfig.java
@@ -1,16 +1,16 @@
-package spring.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/employee/find/**").hasRole("ADMIN").and().httpBasic();
-        http.csrf().disable();
-    }
-}
+//package spring.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests().antMatchers("/employee/find/**").hasRole("ADMIN").and().httpBasic();
+//        http.csrf().disable();
+//    }
+//}
