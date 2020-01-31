92f19e3a471df5b97891fe11b02a4e14acfb9a39
c312882 <C312882@hd00.UNICREDITGROUP.EU>
2020-01-31 16:34:05 +0200

    added a controller for view approach

--- src/main/java/spring/controller/EmployeeController.java
+++ src/main/java/spring/controller/EmployeeController.java
@@ -1,73 +1,37 @@
 package spring.controller;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import spring.config.OwnerConfigProperties;
-import spring.dto.EmployeeDto;
-import spring.exception.EmployeeNotFoundException;
-import spring.model.Employee;
-import spring.service.EmployeeService;
 
-import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
-@RestController
-@RequestMapping(path = "/employee")
+@Controller
+@RequestMapping(path = "/om")
 public class EmployeeController {
-    private EmployeeService employeeService;
-    private ModelMapper modelMapper;
     private OwnerConfigProperties ocp;
 
     @Autowired
-    public EmployeeController(EmployeeService employeeService, ModelMapper modelMapper, OwnerConfigProperties ocp) {
-        this.employeeService = employeeService;
-        this.modelMapper = modelMapper;
+    public void setOcp(OwnerConfigProperties ocp) {
         this.ocp = ocp;
     }
 
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping(path = "/add", consumes = "application/json", produces = "application/json")
-    public Employee create(@RequestBody EmployeeDto employeeDto) throws ParseException {
-        return this.employeeService.save(convertToEntity(employeeDto));
+    @GetMapping(path = "/home")
+    public String home(Model model) {
+        model.addAttribute("name", ocp.getName());
+        model.addAttribute("surname", ocp.getSurname());
+        model.addAttribute("mail", ocp.getMail());
+        return "home";
     }
 
-    @ResponseStatus(HttpStatus.OK)
-    @PutMapping(path = "/update", consumes = "application/json", produces = "application/json")
-    public Employee update(@RequestBody EmployeeDto employeeDto) throws EmployeeNotFoundException, ParseException {
-        Employee employee = convertToEntity(employeeDto);
-        this.employeeService.findById(employee.getId()).orElseThrow(EmployeeNotFoundException::new);
-        return this.employeeService.save(employee);
+    @GetMapping(path = "/signin")
+    public String signin(Model model) {
+        return "signin";
     }
 
-    @DeleteMapping(path = "/delete/id", consumes = "application/json", produces = "application/json")
-    public void delete(@RequestBody EmployeeDto employeeDto) throws EmployeeNotFoundException, ParseException {
-        Employee employee = convertToEntity(employeeDto);
-        this.employeeService.findById(employee.getId()).orElseThrow(EmployeeNotFoundException::new);
-        this.employeeService.deleteById(employee.getId());
-    }
-
-    @PostMapping(path = "/find/wage", consumes = "application/json", produces = "application/json")
-    public Iterable<Employee> getByWage(@RequestBody EmployeeDto employeeDto) throws ParseException {
-        return this.employeeService.findByWage(convertToEntity(employeeDto).getWage());
-    }
-
-    @GetMapping(path = "/find/all", produces = "application/json")
-    public Iterable<EmployeeDto> getAll() {
-        Iterable<Employee> it = this.employeeService.findAll();
-        List<Employee> employees = new ArrayList<>();
-        it.forEach(employees::add);
-        return employees.stream().map(this::convertToDto).collect(Collectors.toList());
-    }
-
-    private EmployeeDto convertToDto(Employee employee) {
-        return this.modelMapper.map(employee, EmployeeDto.class);
-    }
-
-    private Employee convertToEntity(EmployeeDto employeeDto) throws ParseException {
-        return this.modelMapper.map(employeeDto, Employee.class);
+    @GetMapping(path = "/signup")
+    public String signup(Model model) {
+        return "signup";
     }
 }
